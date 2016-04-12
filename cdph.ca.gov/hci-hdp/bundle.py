cff8bc5eff8402d69be20dba14ad744df67253d0
Eric Busboom <eric@clarinova.com>
2016-04-12 08:41:56 -0700

    .

--- cdph.ca.gov/hci-hdp/bundle.py
+++ cdph.ca.gov/hci-hdp/bundle.py
@@ -3,5 +3,39 @@ import ambry.bundle
 
 
 class Bundle(ambry.bundle.Bundle):
-    pass
 
+
+    def meta_copy_partitions(self):
+        from six import text_type
+        from ambry.orm.exc import NotFoundError
+
+        ref_b = self.library.bundle(self.source('hci').ref)
+
+        for p in ref_b.partitions:
+            
+            if p.grain == 'county':
+
+                try:
+                    s = self.source(p.identity.vid)
+                    print s.name, s.ref
+                except NotFoundError:
+                    self.log("Copy in as source: {}".format(p.identity.name))
+                    self.dataset.new_source(name=p.table.name, 
+                                            dest_table_name=p.table.name, 
+                                            reftype='partition', 
+                                            ref=text_type(p.identity.name))
+
+        self.build_source_files.sources.objects_to_record()
+
+        self.commit()
+        
+    def check_strata(self):
+        
+        for p in self.partitions:
+            if 'strata_name' in p.table.header:
+                print '----', p.name
+                
+                df = p.dataframe()
+            
+                print 'strata_name',df.strata_name.unique()
+                print 'strata_level_name',df.strata_name.unique()
--- cdph.ca.gov/hci-hdp/lib.py
+++ cdph.ca.gov/hci-hdp/lib.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 # Ambry Bundle Library File
 # Use this file for code that may be imported into other bundles
+
+def load_groups(b, group_name):
+    
+    with b.source_fs.open('groups.txt') as f:
+        for l in f:
+            l = l.strip()
+            if l.startswith(group_name):
+                _, table = l.split()
+                yield table
