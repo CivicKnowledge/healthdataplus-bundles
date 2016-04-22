b84d86c1abacf8eeeb5842eae2075f4707d22c13
Eric Busboom <eric@clarinova.com>
2016-04-22 16:37:15 -0700

    .

--- census.gov/acs-enrollment-p5ye2014-hdp/bundle.py
+++ census.gov/acs-enrollment-p5ye2014-hdp/bundle.py
@@ -2,77 +2,45 @@
 import ambry.bundle
 
 
+from ambry.bundle.events import *
+
 class Bundle(ambry.bundle.Bundle):
     pass
     
 
-
-    def stderr(self, m):
-        try:
-            return m/1.645
-        except TypeError as e:
-            return None
-
-
-    def rse(self, val, m):
-        try:
-            return round(m/1.645 / val * 100, 2)
-        except (TypeError, ZeroDivisionError) as e:
-            return None
+    def select_county_partition(self, source, row):
+        "Selects a partition for the combined source, as configured in the pipelines in the bundle.yaml file"
+        
+        from ambry.identity import PartialPartitionName
+        
+        return PartialPartitionName(table='prek_enrollment', grain='county',
+                                    space=row.county)
+        
+        
+    def select_county_cols_partition(self, source, row):
+        "Selects a partition for the combined source, as configured in the pipelines in the bundle.yaml file"
+        
+        from ambry.identity import PartialPartitionName
+        
+        return PartialPartitionName(table='prek_enrollment_combined', grain='county',
+                                    space=row.county)
+    
+        
+    @after_build(4)
+    def describe_partitions(self):
+        
+        for p in self.partitions:
+            if p.grain != 'county' or not p.space:
+                self.log("Skipping describe_partitions for {} ".format(p.name))
+                continue
+                
+            p.title = 'Pre-K Enrollment for {} County'.format(p.space.title())
+            
+        self.commit()
     
 
-    def pct(self, val,  total):
-        
-        if not total or total == 0:
-            return None
-        else:
-            return round(float(val)/float(total) * 100 , 2)
 
-    def pctrse(self, val, val_se, total, total_se):
-        from math import sqrt
-        try:
-            
-            r = (val/total) 
-            
-            try:
-                se =  sqrt( (val_se**2 - ( r**2 * total_se**2)) ) / total
-            except ValueError:
-                se = sqrt( (val_se**2 + ( r**2 * total_se**2)) ) / total
-            
-            
-            return round(se / r * 100 , 2)
-            
-        except Exception as e:
-            
-            return None
 
-    def augment_schema(self):
-        
-        t = self.table('b14001')
-        cols = list(t.columns)
-        
-        for c in cols:
-            if c.sequence_id <  11 or c.parent:
-                continue
-              
-
-            t.add_column(
-                name = c.name + '_pct',
-                valuetype = 'm/pct?',
-                parent = c.name,
-                transform = '||bundle.pct(val=row.{}, total=row.total)'.format(c.name),
-                description = 'Percentage of {}, of '.format(c.description)
-            )
-            
-            t.add_column(
-                name = c.name + '_pct_rse',
-                valuetype = 'e/rse?',
-                parent = c.name,
-                transform = '||bundle.pctrse(val=row.{0}, val_se=row.{0}_se, total=row.total, total_se=row.total_se)'.format(c.name),
-                description = 'Relative standard error for percentage of: {}'.format(c.description)
-            )
-            
-        self.update_schema()
         
                 
             
---
+++ census.gov/acs_p5ye2014-poverty-hdp/acs.sql
@@ -0,0 +1,7 @@
+
+MATERIALIZE census.gov-acs-p5ye2014-c18131
+MATERIALIZE census.gov-acs-p5ye2014-c27016
+MATERIALIZE census.gov-acs-p5ye2014-b17020
+
+CREATE VIEW poverty_12mo_age AS
+SELECT * FROM 
