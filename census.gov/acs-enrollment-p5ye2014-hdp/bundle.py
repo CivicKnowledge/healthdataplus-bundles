2e263de1dbc89c1094dd30b67c4179f3047ecba2
Eric Busboom <eric@clarinova.com>
2016-04-26 15:16:35 -0700

    .

--- census.gov/acs-enrollment-p5ye2014-hdp/bundle.py
+++ census.gov/acs-enrollment-p5ye2014-hdp/bundle.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import ambry.bundle
+from ambry.util import memoize
 
 
 from ambry.bundle.events import *
 
 class Bundle(ambry.bundle.Bundle):
     pass
+    
+    
+    @property
+    @memoize
+    def counties(self):
+        from geoid.acs import AcsGeoid
+        from geoid.civick import GVid, State
+        
+        
+        d =  {AcsGeoid.parse(row.geoid).county_name.medium_name:str(AcsGeoid.parse(row.geoid).convert(GVid) )
+                for row in self.dep('geofile50') if row.state == 6}
+         
+        d['California'] = State(6)
+        
+        return d
+         
+         
+    def map_county(self, row):
+        
+        return  self.counties.get(row.region)
+        
+    
+    def mangle_dollars(self, v):
+        
+        try:
+            return str(v).replace('$','').replace(',','')
+        except:
+            return None
     
 
     def select_county_partition(self, source, row):
